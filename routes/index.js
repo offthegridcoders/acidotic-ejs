6944a402c41691557182c0e76bfad75ab0a620f4
Stephen <steveyoung359@gmail.com>
2015-04-10 15:02:00 -0500

    added all races

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var Firebase = require('firebase');
 var fbRef = new Firebase('https://acidotic.firebaseio.com/');
-var fbData;
 
 var defaultSeason = 'winter';
 
@@ -52,11 +51,82 @@ router.get('/contact', function(req, res, next) {
     page: 'contact'});
 });
 
-router.get('/event', function(req, res, next) {
-  fbRef.child('events/summer/10').on('value', function(snapshot) {
-    fbData = snapshot.val();
+// WINTER EVENTS
+router.get('/sidehiller-snowshoe', function(req, res, next) {
+  fbRef.child('events/winter/1').on('value', function(snapshot) {
+    return res.render('pages/single-event', snapshot.val());
   });
-  res.render('pages/single-event', fbData);
+});
+
+router.get('/kingman-farm', function(req, res, next) {
+  fbRef.child('events/winter/2').on('value', function(snapshot) {
+    return res.render('pages/single-event', snapshot.val());
+  });
+});
+
+router.get('/snowshoe-hullabaloo', function(req, res, next) {
+  fbRef.child('events/winter/3').on('value', function(snapshot) {
+    return res.render('pages/single-event', snapshot.val());
+  });
+});
+
+router.get('/snowshoe-championship', function(req, res, next) {
+  fbRef.child('events/winter/4').on('value', function(snapshot) {
+    return res.render('pages/single-event', snapshot.val());
+  });
+});
+
+// SPRING EVENTS
+router.get('/ralph-waldo', function(req, res, next) {
+  fbRef.child('events/spring/6').on('value', function(snapshot) {
+    return res.render('pages/single-event', snapshot.val());
+  });
+});
+
+router.get('/exeter-trail', function(req, res, next) {
+  fbRef.child('events/spring/7').on('value', function(snapshot) {
+    return res.render('pages/single-event', snapshot.val());
+  });
+});
+
+// SUMMER EVENTS
+
+router.get('/loon-mountain-race', function(req, res, next) {
+  fbRef.child('events/summer/8').on('value', function(snapshot) {
+    return res.render('pages/single-event', snapshot.val());
+  });
+});
+
+router.get('/kingman-farm-trail-race', function(req, res, next) {
+  fbRef.child('events/summer/10').on('value', function(snapshot) {
+    return res.render('pages/single-event', snapshot.val());
+  });
+});
+
+router.get('/harmony-hill', function(req, res, next) {
+  fbRef.child('events/summer/11').on('value', function(snapshot) {
+    return res.render('pages/single-event', snapshot.val());
+  });
+});
+
+// FALL EVENTS
+
+router.get('/bretton-woods', function(req, res, next) {
+  fbRef.child('events/fall/12').on('value', function(snapshot) {
+    return res.render('pages/single-event', snapshot.val());
+  });
+});
+
+router.get('/vulcans-fury', function(req, res, next) {
+  fbRef.child('events/fall/13').on('value', function(snapshot) {
+    return res.render('pages/single-event', snapshot.val());
+  });
+});
+
+router.get('/roaring-falls', function(req, res, next) {
+  fbRef.child('events/fall/14').on('value', function(snapshot) {
+    return res.render('pages/single-event', snapshot.val());
+  });
 });
 
 module.exports = router;
