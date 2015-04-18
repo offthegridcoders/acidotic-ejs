62df3b9e9f661a59b31d4198c7b8076cad633f09
Stephen <steveyoung359@gmail.com>
2015-04-18 13:40:13 -0400

    updated about image hover

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -3,3 +3,13 @@ function headerIsOffScreen() {
   // -100px since header images fade out
   return $(window).scrollTop() > (parseInt($('header').css('height'), 10) - 300)
 };
+
+
+// Image Hover Changes in About Page
+function changeBackgroundImage(id, img) {
+  $('#' + id).css("background-image", img);
+};
+
+$(".thumb-pics").hover(function() {
+  changeBackgroundImage('whyBlock', $(this).css("background-image"));
+});
