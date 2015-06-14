ebdbbba52d6ae94734f2de8ff81505f6c60a5fdf
Stephen <steveyoung359@gmail.com>
2015-06-14 13:51:06 -0400

    updated gulpfile and concat js, minified images, etc...

--- gulpfile.js
+++ gulpfile.js
@@ -7,18 +7,49 @@ var autoprefixer = require('gulp-autoprefixer');
 var concatCss = require('gulp-concat-css');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
+var concat = require('gulp-concat');
 
-var paths = {
+var mainCssName = 'style.css';
+var mainJsName = 'build.js';
+
+var allFiles = {
+  css: 'public/scss/*.css',
   scss: 'public/scss/**/*.scss',
-  assets: 'public/assets/**/*',
-  vendor: 'public/scss/vendor/*.css'
+  assets: 'public/assets/**/*.*',
+  vendorScss: 'public/scss/vendor/*.css',
+  js: 'public/js/**/*.js'
 };
 
-gulp.task('default', ['sass', 'img-min'], function() {
+var paths = {
+  scss: './public/scss/',
+  css: './public/dist/',
+  dist: './public/dist/',
+  assets: './public/assets/'
+};
+
+var files = {
+  js: {
+    all:
+      [
+        './bower_components/jquery/dist/jquery.min.js',
+        './bower_components/wow/dist/wow.min.js',
+        './public/js/inits.js',
+        './public/js/functions.js',
+        './public/js/scripts.js',
+        './public/js/parallax.js'
+      ],
+    main: './public/dist/' + mainJsName
+  },
+  css: './public/dist/' + mainCssName
+}
+
+gulp.task('default', ['sass', 'img-min', 'concat-scripts'], function() {
+  // watchs files for changes to rebuild
   return gulp.watch([
-      paths.scss,
-      paths.assets,
-      paths.vendor,
+      allFiles.scss,
+      allFiles.assets,
+      allFiles.vendorScss,
+      allFiles.js
     ], ['default']);
 });
 
@@ -28,40 +59,52 @@ gulp.task('default', ['sass', 'img-min'], function() {
 
   gulp.task('sass', ['minify-css']);
 
+  // compiles scss files into css
   gulp.task('compile-sass', function() {
-    return gulp.src(paths.scss)
+    return gulp.src(allFiles.scss)
       .pipe(scsslint())
       .pipe(sass())
-      .pipe(gulp.dest('./public/scss/'));
+      .pipe(gulp.dest(paths.scss));
   });
 
+  // concatenates all css files
   gulp.task('concat-css', ['compile-sass'], function () {
-    return gulp.src(['./public/scss/*.css', paths.vendor])
-      .pipe(concatCss('style.css'))
-      .pipe(gulp.dest('./public/stylesheets'));
+    return gulp.src([allFiles.css, allFiles.vendorScss])
+      .pipe(concatCss(mainCssName))
+      .pipe(gulp.dest(paths.css));
   });
   
+  // auto prefixes css properties
   gulp.task('auto-prefix', ['concat-css'], function () {
-    return gulp.src('./public/stylesheets/style.css')
+    return gulp.src(files.css)
         .pipe(autoprefixer({
             browsers: ['last 10 versions'],
             cascade: true
         }))
-        .pipe(gulp.dest('./public/stylesheets/'));
+        .pipe(gulp.dest(paths.css));
   });
 
+  // minifies all css
   gulp.task('minify-css', ['auto-prefix'], function() {
-    return gulp.src('./public/stylesheets/style.css')
+    return gulp.src(files.css)
       .pipe(minifyCSS())
-      .pipe(gulp.dest('./public/stylesheets/'))
+      .pipe(gulp.dest(paths.css))
+  });
+
+  // concatenates all scripts
+  gulp.task('concat-scripts', function() {
+  return gulp.src(files.js.all)
+    .pipe(concat(mainJsName))
+    .pipe(gulp.dest(paths.dist));
   });
   
+  // minifies all images
   gulp.task('img-min', function () {
-    return gulp.src('./public/assets/*.*')
+    return gulp.src(allFiles.assets)
       .pipe(imagemin({
         progressive: true,
         svgoPlugins: [{removeViewBox: false}],
         use: [pngquant()]
       }))
-      .pipe(gulp.dest('./public/assets/'));
+      .pipe(gulp.dest(paths.assets));
   });
